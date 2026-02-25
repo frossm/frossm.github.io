58e797a6732c90ce8f0f57bc53f26cad095cdfd9
Michael Fross <michael@fross.org>
2026-02-24 19:31:58 -0600

    Added PHIBox constant

--- RPNCalc-UserGuide/searcher-c2a407aa.js
+++ RPNCalc-UserGuide/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-80b0c864.js',
+                path_to_root + 'searchindex-e0ae0b67.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
